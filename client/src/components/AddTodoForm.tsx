55268da8cf9932ec2b6bccfe5b4bacdbac96cdf0
Mwero Abdalla <mwerothewebmaker@gmail.com>
2025-06-19 22:33:03 +0300

    chore: add update logic

---
+++ client/src/components/AddTodoForm.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import type { ITodo } from "./TodoItem";
+
+interface AddTodoFormProps {
+  todo: ITodo | null;
+  setTodos: any;
+  todos: ITodo[];
+}
+
+export function AddTodoForm({ todo, setTodos, todos }: AddTodoFormProps) {
+  const [newTodo, setNewTodo] = useState<Partial<ITodo>>({ title: "" });
+  useEffect(()=>{
+    // console.log({todo})
+    setNewTodo(todo||{title:""})
+  },[todo])
+// console.log({newTodo})
+  // handle change
+  function handleChange(e: any) {
+    setNewTodo({ ...newTodo, title: e.target.value });
+    console.log(newTodo);
+  }
+
+  function handleSubmit() {
+    if (!newTodo.title) return;
+    fetch(`http://localhost:5000/todos${newTodo._id?"/"+newTodo._id : ""} `, {
+      method:newTodo?._id?"PUT": "POST",
+      body: JSON.stringify(newTodo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((body) => {
+        // console.log({body})
+        if (!body.success) throw Error("An Error has occured!");
+       
+        if (body.data){
+          if(newTodo._id) return setTodos(todos.map(t=>t._id===newTodo._id?body.data:t))
+           setTodos([body.data, ...todos]);
+          }
+        alert("Todo Added Successfully");
+        
+        setNewTodo({ title: "" });
+      })
+      .catch((error: any) => console.log("Error " + error.message));
+  }
+
+  return (
+    <div className="p-4 flex justify-between">
+      <input
+          value={newTodo.title}
+        onChange={handleChange}
+        onKeyDown={(e)=>{
+            
+            if(e.code.toLowerCase() !== 'enter') return
+            handleSubmit()
+        }}
+        className="px-6 py-3 bg-slate-50 text-black rounded-md"
+      />
+      <button
+        onClick={() => handleSubmit()}
+        className="px-4 py-2 rounded bg-green-500 text-white"
+      >
+        Save
+      </button>
+    </div>
+  );
+}
